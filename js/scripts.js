8dab24aa200fcbc4d7d438820318afcf7a0fba1f
Krisztina Pap <krisztina.pap@outlook.com>
2020-08-31 12:21:37 -0600

    Turned 'front-end' skills heading into a button that hides/shows list below;

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,23 @@ function sendMail() {
     contactForm.reset();
 };
 
+// const myMenu = document.getElementById("myMenu");
+// const mySkills = document.getElementById("mySkills");
+const frontEnd = document.getElementById("frontEnd");
+const backEnd = document.getElementById("backEnd");
+const databases = document.getElementById("databases");
+const devOps = document.getElementById("devOps");
+const dataScience = document.getElementById("dataScience");
+const softSkills = document.getElementById("softSkills");
+
+const freelancer = document.getElementById("freelancer");
+const eslInstructor = document.getElementById("eslInstructor");
+const executiveAssociate = document.getElementById("executiveAssociate");
+const timeEntryLead = document.getElementById("timeEntryLead");
+const trainingSpecialist = document.getElementById("trainingSpecialist");
+
+const certificates = document.getElementById("certificates");
+
 //Citation:
 //      https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar
 //      The below code block hides/shows my nav menu
@@ -27,4 +44,17 @@ function myMenu() {
     } else {
       x.style.display = "block";
     }
+}
+
+//Citation:
+//      https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar
+//      The below code block hides/shows my nav menu
+function myFrontEndSkills() {
+  let x = frontEnd;
+  console.log(frontEnd);
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
 }
