bc6c99bce4cee2f3ad13cfaedf80118249a80864
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-01 10:05:51 -0600

    Adds swear word validaton feature; Changes the logo from transparent to solid white background;

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ September 1, 2020
 - [StackOverflow: mailto using javascript](https://stackoverflow.com/questions/21028939/mailto-using-javascript)
 - [Webdeveloper.com: Javascript Mailto form](https://www.webdeveloper.com/d/229947-javascript-mailto-form)
 - [w3schools: Mobile Navbar](https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar)
+- [StackOverflow: Remove all special characters with RegExp](https://stackoverflow.com/questions/4374822/remove-all-special-characters-with-regexp)
 - [Pixabay: programming photo (no attribution required)](https://pixabay.com/illustrations/web-network-programming-3706562/)
 - [Pixabay: photo of laptop (no attribution required)](https://pixabay.com/photos/keyboard-computer-technology-light-5017973/)
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,59 @@
 const contactForm = document.getElementById( "contactForm" );
 const myEmail = "krisztina.pap@outlook.com";
+let isMessageBad = false;
 
 
 function sendMail() {
     const eAddress = document.getElementById( "emailAddress" ).value;
     const eSubject = document.getElementById( "emailSubject" ).value;
     const eBody = document.getElementById( "emailBody" ).value;
+    
+    let badWordArray = [
+      "feldercarb",   // Swear word in Battlestar Galactica
+      "frack",        // Swear word in Battlestar Galactica
+      "skinjob",      // Swear word in Battlestar Galactica
+      "vulgacarb",    // Swear word in Battlestar Galactica
+      "goram",
+      "gorram",       // Swear word in Firefly/Serenity 
+      "guay",         // Swear word in Firefly/Serenity 
+      "bizui",        // Swear word in Firefly/Serenity 
+      "bhijoi"        // Swear word in Firefly/Serenity 
+    ];
+
 
     // Citation:
-    //      https://stackoverflow.com/questions/21028939/mailto-using-javascript
-    //      https://www.webdeveloper.com/d/229947-javascript-mailto-form
-    //      The below two lines of code build the email message from the variables we prompted the user for and then we use the user's email software in a new window to send the message
-    const yourEmailMessage = `mailto:${myEmail}?cc=${eAddress}&subject=${eSubject}&body=${eBody}`;
-    window = window.open(yourEmailMessage, 'emailWindow');
+    //    https://stackoverflow.com/questions/4374822/remove-all-special-characters-with-regexp
+    // The below line of code replaces all characters except for lower and uppercase letters, numbers, the character "'", spaces, and tabs. The caret means "everything else" and the "gi" means "global" and "case insensitive"
+  
+    let cleanText = eBody.replace( /[^a-zA-Z0-9' \t]/gi, "" ); // 
+    let userMessage = cleanText.split(" ");
 
+    for ( let i = 0; i < userMessage.length; i++ )
+    {
+      if ( badWordArray.includes( userMessage[i] ))
+      {
+        isMessageBad = true;
+      }
+    }
+
+    if ( isMessageBad === true )
+    {
+      alert("Let's stay professional, please!");
+    }
+    else {
+      // Citation:
+      //      https://stackoverflow.com/questions/21028939/mailto-using-javascript
+      //      https://www.webdeveloper.com/d/229947-javascript-mailto-form
+      //      The below two lines of code build the email message from the variables we prompted the user for and then we use the user's email software in a new window to send the message
+      const yourEmailMessage = `mailto:${myEmail}?cc=${eAddress}&subject=${eSubject}&body=${eBody}`;
+      window = window.open(yourEmailMessage, 'emailWindow');
+    }
+    isMessageBad = false;
     contactForm.reset();
 };
 
-// const myMenu = document.getElementById("myMenu");
-// const mySkills = document.getElementById("mySkills");
+
+
 const frontEnd = document.getElementById("frontEnd");
 const backEnd = document.getElementById("backEnd");
 const databases = document.getElementById("databases");
@@ -58,3 +93,4 @@ function showHideMyInfo( button ) {
     x.style.display = "block";
   }
 }
+
