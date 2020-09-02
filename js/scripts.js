39da9367df3657f95d4b9f1e0dcc7bf6ff897f7b
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-02 17:24:00 -0600

    Cleaned up and commented JS file;

--- js/scripts.js
+++ js/scripts.js
@@ -1,76 +1,66 @@
-const contactForm = document.getElementById( "contactForm" );
-const myEmail = "krisztina.pap@outlook.com";
-let isMessageBad = false;
-
+// SendMail function
+//    Inputs: values of user email, subject, and message input fields
+//    Outputs: (if passes the "bad words" test); A pre-populated new email in the user's email client
+//    Purpose: To take the information the user gives us, validate it, then put it into a new email so the user only has to press send to email us.
 
 function sendMail() {
-    const eAddress = document.getElementById( "emailAddress" ).value;
-    const eSubject = document.getElementById( "emailSubject" ).value;
-    const eBody = document.getElementById( "emailBody" ).value;
-    
-    let badWordArray = [
-      "feldercarb",   // Swear word in Battlestar Galactica
-      "frack",        // Swear word in Battlestar Galactica
-      "skinjob",      // Swear word in Battlestar Galactica
-      "vulgacarb",    // Swear word in Battlestar Galactica
-      "goram",
-      "gorram",       // Swear word in Firefly/Serenity 
-      "guay",         // Swear word in Firefly/Serenity 
-      "bizui",        // Swear word in Firefly/Serenity 
-      "bhijoi"        // Swear word in Firefly/Serenity 
-    ];
+  const contactForm = document.getElementById( "contactForm" );
+  const eAddress = document.getElementById( "emailAddress" ).value;
+  const eSubject = document.getElementById( "emailSubject" ).value;
+  const eBody = document.getElementById( "emailBody" ).value;
+  const myEmail = "krisztina.pap@outlook.com";
+  let isMessageBad = false;
+  let badWordArray = [
+    "feldercarb",   // Swear word in Battlestar Galactica
+    "frack",        // Swear word in Battlestar Galactica
+    "skinjob",      // Swear word in Battlestar Galactica
+    "vulgacarb",    // Swear word in Battlestar Galactica
+    "goram",
+    "gorram",       // Swear word in Firefly/Serenity 
+    "guay",         // Swear word in Firefly/Serenity 
+    "bizui",        // Swear word in Firefly/Serenity 
+    "bhijoi"        // Swear word in Firefly/Serenity 
+  ];
 
+  // Citation:
+  //    https://stackoverflow.com/questions/4374822/remove-all-special-characters-with-regexp
+  // The below line of code replaces all characters except for lower and uppercase letters, numbers, the character "'", spaces, and tabs. The caret means "everything else" and the "gi" means "global" and "case insensitive"
 
+  let cleanText = eBody.replace( /[^a-zA-Z0-9' \t]/gi, "" ); // 
+  let userMessage = cleanText.split(" ");
+
+  for ( let i = 0; i < userMessage.length; i++ )
+  {
+    if ( badWordArray.includes( userMessage[i] ))
+    {
+      isMessageBad = true;
+    }
+  }
+
+  if ( isMessageBad === true )
+  {
+    alert("Let's stay professional, please!");
+  }
+  else {
     // Citation:
-    //    https://stackoverflow.com/questions/4374822/remove-all-special-characters-with-regexp
-    // The below line of code replaces all characters except for lower and uppercase letters, numbers, the character "'", spaces, and tabs. The caret means "everything else" and the "gi" means "global" and "case insensitive"
-  
-    let cleanText = eBody.replace( /[^a-zA-Z0-9' \t]/gi, "" ); // 
-    let userMessage = cleanText.split(" ");
-
-    for ( let i = 0; i < userMessage.length; i++ )
-    {
-      if ( badWordArray.includes( userMessage[i] ))
-      {
-        isMessageBad = true;
-      }
-    }
-
-    if ( isMessageBad === true )
-    {
-      alert("Let's stay professional, please!");
-    }
-    else {
-      // Citation:
-      //      https://stackoverflow.com/questions/21028939/mailto-using-javascript
-      //      https://www.webdeveloper.com/d/229947-javascript-mailto-form
-      //      The below two lines of code build the email message from the variables we prompted the user for and then we use the user's email software in a new window to send the message
-      const yourEmailMessage = `mailto:${myEmail}?cc=${eAddress}&subject=${eSubject}&body=${eBody}`;
-      // Citation:
-      //      https://stackoverflow.com/questions/21461589/javascript-mailto-using-window-open
-      //      Further improved the form by not opening a new browser tab every time the user clicks send.
-      location.href = yourEmailMessage;
-    }
-    isMessageBad = false;
-    contactForm.reset();
+    //      https://stackoverflow.com/questions/21028939/mailto-using-javascript
+    //      https://www.webdeveloper.com/d/229947-javascript-mailto-form
+    //      The below two lines of code build the email message from the variables we prompted the user for and then we use the user's email software in a new window to send the message
+    const yourEmailMessage = `mailto:${myEmail}?cc=${eAddress}&subject=${eSubject}&body=${eBody}`;
+    // Citation:
+    //      https://stackoverflow.com/questions/21461589/javascript-mailto-using-window-open
+    //      Further improved the form by not opening a new browser tab every time the user clicks send
+    location.href = yourEmailMessage;
+  }
+  isMessageBad = false;
+  contactForm.reset();
 };
 
-const allSkillLists = document.querySelectorAll("#skills-anchor li");
 
-const frontEnd = document.getElementById("frontEnd");
-const backEnd = document.getElementById("backEnd");
-const databases = document.getElementById("databases");
-const devOps = document.getElementById("devOps");
-const dataScience = document.getElementById("dataScience");
-const softSkills = document.getElementById("softSkills");
-
-const freelancer = document.getElementById("freelancer");
-const eslInstructor = document.getElementById("eslInstructor");
-const executiveAssociate = document.getElementById("executiveAssociate");
-const timeEntryLead = document.getElementById("timeEntryLead");
-const trainingSpecialist = document.getElementById("trainingSpecialist");
-
-const certificates = document.getElementById("certificates");
+// MyMenu function
+//    Inputs: Buttonclick
+//    Outputs: The style display attribute toggles between "block" and "none" on the nav items
+//    Purpose: To hide and show the menu items when the user clicks on the menu button
 
 //Citation:
 //      https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar
@@ -84,6 +74,11 @@ function myMenu() {
     }
 }
 
+// showHideMyInfo function
+//    Inputs: Buttonclick
+//    Outputs:  The style display attribute toggles between "block" and "none" on the lists under the expanding buttons
+//    Purpose: To show and hide the skills, work experience, and certificates detailed information
+
 //Citation:
 //      https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_mobile_navbar
 //      The below code block was modified to show/hide blocks of the code (expanding buttons)
@@ -96,5 +91,3 @@ function showHideMyInfo( button ) {
     x.style.display = "block";
   }
 }
-
-
